41a908d252d7861142231597ea2da2c98bd04927
Carlos Andrés Cortés Vera <carloscortesvera11@gmail.com>
2023-06-18 10:11:43 -0500

    pruebascon

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 var express = require("express");
 const app = express();
 const path = require("path");
-//primero declaramos las contates para llamar express y la app//
-//configuraciones//
+//primero declaramos las contates para llamar express y la app
+//configuraciones
 app.set("puerto",3000);
-app.set("views","C:/Users/carlo/OneDrive/Escritorio/paginaweb/src/views","views");
+app.set('views', path.join(__dirname, 'views'));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
+//
 app.get("/",(req,res) =>{
     res.render("index.ejs",{titulo:"Inicio"});
 });
@@ -20,7 +21,6 @@ app.get("/blog",(req,res) =>{
 app.get("/contacto",(req,res) =>{
     res.render("contacto.ejs",{titulo:"Contacto"});
 });
-
 
 
 app.use(express.static("C:/Users/carlo/OneDrive/Escritorio/paginaweb/src/public"));
